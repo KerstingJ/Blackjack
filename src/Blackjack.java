049913f852497a61bd2904c48f2cfddebc664d3f
KerstingJ <kersting.josh@gmail.com>
2016-09-25 00:51:23 -0500

    few tweaks

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -45,7 +45,6 @@ public class Blackjack {
 			while (response < 1 && p.blackJack == false) {
 
 				if (p.handScore() > 21) {
-					System.out.println("Sorry, Bust");
 					break;
 				}
 
@@ -56,7 +55,7 @@ public class Blackjack {
 				if (response == 0) {
 					p.drawCard();
 				}
-
+				System.out.println("Drew a " + p.hand.get(p.hand.size()-1));
 				System.out.println("Hand score: " + p.handScore());
 			}
 
